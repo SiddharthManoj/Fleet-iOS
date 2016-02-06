03d8b6e2cdd88bbe5b3d3689b89b18179dec152b
Spencer Congero <congero@usc.edu>
2016-02-05 19:11:35 -0800

    profile page layout

--- Fleet/Fleet/ProfileViewController.swift
+++ Fleet/Fleet/ProfileViewController.swift
@@ -19,8 +19,8 @@ class ProfileViewController: UIViewController, UITextFieldDelegate
     var fleetColorBlue: CGFloat = 177/255
     
     var settingsYPos: CGFloat = 300
-    var settingsWidth: CGFloat = 200
-    var settingsHeight: CGFloat = 80
+    var settingsWidth: CGFloat = 50
+    var settingsHeight: CGFloat = 50
     
     var quicksandReg: String = "Quicksand-Regular"
     var quicksandBold: String = "Quicksand-Bold"
@@ -37,6 +37,24 @@ class ProfileViewController: UIViewController, UITextFieldDelegate
     
     var profileLabel: UILabel!
     var settingsButton: UIButton!
+    var nameLabel: UILabel!
+    var pointsNumLabel: UILabel!
+    var pointsLabel: UILabel!
+    var followersNumLabel: UILabel!
+    var followersLabel: UILabel!
+    
+    var postsLabel: UILabel!
+    var annotationsLabel: UILabel!
+    
+    var postsBorderTop = CALayer()
+    var postsBorderBottom = CALayer()
+    
+    var annotationsBorderTop = CALayer()
+    var annotationsBorderBottom = CALayer()
+    
+    var postsContainer: UIView!
+    var annotationsContainer: UIView!
+    
     
     // MARK: - UIViewController methods
     
@@ -47,19 +65,11 @@ class ProfileViewController: UIViewController, UITextFieldDelegate
         let bgColor = UIColor(red: bgColorRed, green: bgColorGreen, blue: bgColorBlue, alpha: 1)
         self.view.backgroundColor = bgColor
         
-        // quickly just for testing, will delete later
-        
-        self.profileLabel = UILabel(frame: CGRect(x: self.view.center.x - 200/2, y: 100, width: 200, height: 50))
-        self.profileLabel.attributedText = NSAttributedString(string: "profile", attributes: [NSForegroundColorAttributeName: UIColor(red: fleetColorRed, green: fleetColorGreen, blue: fleetColorBlue, alpha: 1), NSFontAttributeName: UIFont(name: corbertReg, size: 50)!])
-        self.profileLabel.textAlignment = .Center
-        self.profileLabel.backgroundColor = UIColor(white: 1, alpha: 0)
-        
-        self.view.addSubview(profileLabel)
-        
-        /////
-        
         
         _addSettingsButton()
+        _addProfile()
+        _addPosts()
+        _addAnnotations()
 
         
     }
@@ -77,14 +87,133 @@ class ProfileViewController: UIViewController, UITextFieldDelegate
     private func _addSettingsButton()
     {
         self.settingsButton = UIButton()
-        self.settingsButton.setTitle(settingsString, forState: .Normal)
-        self.settingsButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        self.settingsButton.titleLabel?.font = UIFont(name: quicksandReg, size: 20)
-        self.settingsButton.frame = CGRectMake(self.view.center.x - settingsWidth/2, settingsYPos, settingsWidth, settingsHeight)
-        self.settingsButton.backgroundColor = UIColor(red: fleetColorRed, green: fleetColorGreen, blue: fleetColorBlue, alpha: 1)
+        self.settingsButton.setImage(UIImage(named: "settings_icon.png"), forState: .Normal)
+        self.settingsButton.frame = CGRectMake(310, 30, settingsWidth, settingsHeight)
         self.settingsButton.addTarget(self, action: "settingsPressed:", forControlEvents: .TouchUpInside)
         
         self.view.addSubview(self.settingsButton)
+    }
+    
+    private func _addProfile()
+    {
+        // profile circle
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: 70,y: 120), radius: CGFloat(58), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.CGPath
+
+        shapeLayer.fillColor = UIColor(white: 0.96, alpha: 1).CGColor
+        
+        view.layer.addSublayer(shapeLayer)
+        
+        // name
+        self.nameLabel = UILabel(frame: CGRect(x: 140, y: 85, width: 250, height: 40))
+        self.nameLabel.attributedText = NSAttributedString(string: "User Name", attributes: [NSForegroundColorAttributeName: UIColor.blackColor(), NSFontAttributeName: UIFont(name: quicksandReg, size: 22)!])
+        self.nameLabel.backgroundColor = UIColor(white: 1, alpha: 0)
+        self.nameLabel.textAlignment = .Left
+        
+        self.view.addSubview(nameLabel)
+        
+        // points
+        self.pointsNumLabel = UILabel(frame: CGRect(x: 142, y: 121, width: 40, height: 40))
+        self.pointsNumLabel.attributedText = NSAttributedString(string: "279", attributes: [NSForegroundColorAttributeName: UIColor(red: fleetColorRed, green: fleetColorGreen, blue: fleetColorBlue, alpha: 1), NSFontAttributeName: UIFont(name: quicksandReg, size: 20)!])
+        self.pointsNumLabel.backgroundColor = UIColor(white: 1, alpha: 0)
+        self.pointsNumLabel.textAlignment = .Left
+        
+        self.pointsLabel = UILabel(frame: CGRect(x: 186, y: 121, width: 100, height: 40))
+        self.pointsLabel.attributedText = NSAttributedString(string: "points", attributes: [NSForegroundColorAttributeName: UIColor.blackColor(), NSFontAttributeName: UIFont(name: quicksandReg, size: 20)!])
+        self.pointsLabel.backgroundColor = UIColor(white: 1, alpha: 0)
+        self.pointsLabel.textAlignment = .Left
+        
+        self.view.addSubview(pointsNumLabel)
+        self.view.addSubview(pointsLabel)
+        
+        // followers
+        
+        self.followersNumLabel = UILabel(frame: CGRect(x: 142, y: 145, width: 40, height: 40))
+        self.followersNumLabel.attributedText = NSAttributedString(string: "142", attributes: [NSForegroundColorAttributeName: UIColor(red: fleetColorRed, green: fleetColorGreen, blue: fleetColorBlue, alpha: 1), NSFontAttributeName: UIFont(name: quicksandReg, size: 20)!])
+        self.followersNumLabel.backgroundColor = UIColor(white: 1, alpha: 0)
+        self.followersNumLabel.textAlignment = .Left
+        
+        self.followersLabel = UILabel(frame: CGRect(x: 186, y: 145, width: 100, height: 40))
+        self.followersLabel.attributedText = NSAttributedString(string: "followers", attributes: [NSForegroundColorAttributeName: UIColor.blackColor(), NSFontAttributeName: UIFont(name: quicksandReg, size: 20)!])
+        self.followersLabel.backgroundColor = UIColor(white: 1, alpha: 0)
+        self.followersLabel.textAlignment = .Left
+        
+        self.view.addSubview(followersNumLabel)
+        self.view.addSubview(followersLabel)
+        
+    }
+    
+    private func _addPosts()
+    {
+        // posts label
+        self.postsLabel = UILabel(frame: CGRect(x: 15, y: 225, width: 100, height: 30))
+        self.postsLabel.attributedText = NSAttributedString(string: "Posts", attributes: [NSForegroundColorAttributeName: UIColor.blackColor(), NSFontAttributeName: UIFont(name: quicksandReg, size: 22)!])
+        self.postsLabel.backgroundColor = UIColor(white: 1, alpha: 0)
+        self.postsLabel.textAlignment = .Left
+        
+        self.view.addSubview(postsLabel)
+        
+        // container
+        self.postsContainer = UIView(frame: CGRect(x: 0, y: 262, width: self.view.frame.width, height: 136))
+        self.postsContainer.backgroundColor = UIColor(white: 0.98, alpha: 1)
+        let width = CGFloat(1.0)
+        self.postsBorderTop.borderColor = UIColor(red: fleetColorRed, green: fleetColorGreen, blue: fleetColorBlue, alpha: 1).CGColor
+        self.postsBorderTop.frame = CGRect(x: 0, y: width, width: postsContainer.frame.width, height: width)
+        
+        self.postsBorderTop.borderWidth = width
+        self.postsContainer.layer.addSublayer(self.postsBorderTop)
+        self.postsContainer.layer.masksToBounds = true
+        
+        self.postsBorderBottom.borderColor = UIColor(red: fleetColorRed, green: fleetColorGreen, blue: fleetColorBlue, alpha: 1).CGColor
+        self.postsBorderBottom.frame = CGRect(x: 0, y: postsContainer.frame.height - width, width:  postsContainer.frame.width, height: postsContainer.frame.height)
+        
+        self.postsBorderBottom.borderWidth = width
+        self.postsContainer.layer.addSublayer(self.postsBorderBottom)
+        self.postsContainer.layer.masksToBounds = true
+        
+        self.view.addSubview(postsContainer)
+        
+        
+        
+        // videos
+        
+        
+    }
+    
+    private func _addAnnotations()
+    {
+        // annotations label
+        self.annotationsLabel = UILabel(frame: CGRect(x: 15, y: 429, width: 200, height: 30))
+        self.annotationsLabel.attributedText = NSAttributedString(string: "Annotations", attributes: [NSForegroundColorAttributeName: UIColor.blackColor(), NSFontAttributeName: UIFont(name: quicksandReg, size: 22)!])
+        self.annotationsLabel.backgroundColor = UIColor(white: 1, alpha: 0)
+        self.annotationsLabel.textAlignment = .Left
+        
+        self.view.addSubview(annotationsLabel)
+        
+        // container
+        self.annotationsContainer = UIView(frame: CGRect(x: 0, y: 466, width: self.view.frame.width, height: 136))
+        self.annotationsContainer.backgroundColor = UIColor(white: 0.98, alpha: 1)
+        let width = CGFloat(1.0)
+        self.annotationsBorderTop.borderColor = UIColor(red: fleetColorRed, green: fleetColorGreen, blue: fleetColorBlue, alpha: 1).CGColor
+        self.annotationsBorderTop.frame = CGRect(x: 0, y: width, width: annotationsContainer.frame.width, height: width)
+        
+        self.annotationsBorderTop.borderWidth = width
+        self.annotationsContainer.layer.addSublayer(self.annotationsBorderTop)
+        self.annotationsContainer.layer.masksToBounds = true
+        
+        self.annotationsBorderBottom.borderColor = UIColor(red: fleetColorRed, green: fleetColorGreen, blue: fleetColorBlue, alpha: 1).CGColor
+        self.annotationsBorderBottom.frame = CGRect(x: 0, y: annotationsContainer.frame.height - width, width:  annotationsContainer.frame.width, height: annotationsContainer.frame.height)
+        
+        self.annotationsBorderBottom.borderWidth = width
+        self.annotationsContainer.layer.addSublayer(self.annotationsBorderBottom)
+        self.annotationsContainer.layer.masksToBounds = true
+        
+        self.view.addSubview(annotationsContainer)
+        
+        
+        // videos
     }
     
 }
